1caacaf22ce53e43cdc3aa6a2b01774abc36ddba
fahadBadar <fahad.badar@codurance.com>
2024-03-19 11:49:08 +0000

    refactor: added permaterised tests

--- src/test/java/org/example/RomanNumeralsShould.java
+++ src/test/java/org/example/RomanNumeralsShould.java
@@ -1,24 +1,15 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralsShould {
-
-    @Test
-    void convert_1_to_I(){
-        assertEquals("I",RomanNumerals.convert(1));
+    @ParameterizedTest (name = "{0} into {1}")
+    @CsvSource({"1,I","2,II","3,III"})
+    void convert_arabic_number_to_roman_numeral(int input, String expectedOutput){
+        assertEquals(expectedOutput,RomanNumerals.convert(input));
     }
-
-    @Test
-    void convert_2_to_II() {
-        assertEquals("II", RomanNumerals.convert(2));
-    }
-
-    @Test
-    void convert_3_to_III() {
-        assertEquals("III", RomanNumerals.convert(3));
-    }
-
 }
