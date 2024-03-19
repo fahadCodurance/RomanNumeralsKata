1908803b990b4e81a174d0883867d451b2a4722a
fahadBadar <fahad.badar@codurance.com>
2024-03-19 11:54:26 +0000

    feat: convert 5 to V

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -5,6 +5,9 @@ public class RomanNumerals {
     public static String convert(int arabicNumber) {
         String character = "I";
         String result = character;
+        if (arabicNumber == 5) {
+            return "V";
+        }
         if (arabicNumber >= 3){
             result += character;
         }
