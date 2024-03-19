40d5c31c239adf4173bf9601a60a2ef3b58db051
fahadBadar <fahad.badar@codurance.com>
2024-03-19 11:54:10 +0000

    test: convert 5 to V

--- src/test/java/org/example/RomanNumeralsShould.java
+++ src/test/java/org/example/RomanNumeralsShould.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralsShould {
     @ParameterizedTest (name = "{0} into {1}")
-    @CsvSource({"1,I","2,II","3,III"})
+    @CsvSource({"1,I","2,II","3,III","5,V"})
     void convert_arabic_number_to_roman_numeral(int input, String expectedOutput){
         assertEquals(expectedOutput,RomanNumerals.convert(input));
     }
